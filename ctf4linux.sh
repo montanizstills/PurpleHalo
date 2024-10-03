5a9c9552b34b04a9c34fd17f9e51e60759b76a56
montanizstills <montanizstills@gmail.com>
2024-10-03 15:57:50 -0400

    add ctf libraries

---
+++ ctf4linux.sh
@@ -0,0 +1,28 @@
+#!/bin/zsh
+
+sudo apt install install nmap
+snap install enum4linux
+sudo apt install install smbclient
+sudo apt install install smbmap
+sudo apt install install crackmapexec
+sudo apt install install john
+sudo apt install install hydra
+sudo apt install ghidra
+sudo apt install install hashcat
+sudo apt install install nikto
+sudo apt install install gobuster
+sudo apt install install dirb
+sudo apt install install wpscan
+sudo apt install install sqlmap
+sudo apt install install burpsuite
+sudo apt install install metasploit
+sudo apt install owasp-zap
+sudo apt install wpscanteam/tap/wpscan
+sudo apt install exploitdb
+sudo apt install sambabrew install binwalk
+sudo apt install exiftool
+sudo apt install airckrack-ng
+sudo apt install john
+
+
+
