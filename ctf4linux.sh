086e186bc045e7b41472c0e30226b4b824c3fed0
montanizstills <montanizstills@gmail.com>
2024-10-13 18:36:00 -0400

    update script to auto accept EULA;

--- ctf4linux.sh
+++ ctf4linux.sh
@@ -1,28 +1,28 @@
 #!/bin/zsh
 
-sudo apt install nmap
-snap install enum4linux
-sudo apt install smbclient
-sudo apt install smbmap
-sudo apt install crackmapexec
-sudo apt install john
-sudo apt install hydra
-sudo apt install ghidra
-sudo apt install hashcat
-sudo apt install nikto
-sudo apt install gobuster
-sudo apt install dirb
-sudo apt install wpscan
-sudo apt install sqlmap
-sudo apt install burpsuite
-sudo apt install metasploit
-sudo apt install owasp-zap
-sudo apt install wpscanteam/tap/wpscan
-sudo apt install exploitdb
-sudo apt install sambabrew install binwalk
-sudo apt install exiftool
-sudo apt install airckrack-ng
-sudo apt install john
+sudo apt install -y nmap
+snap install -y enum4linux
+sudo apt install -y smbclient
+sudo apt install -y smbmap
+sudo apt install -y crackmapexec
+sudo apt install -y john
+sudo apt install -y hydra
+sudo apt install -y ghidra
+sudo apt install -y hashcat
+sudo apt install -y nikto
+sudo apt install -y gobuster
+sudo apt install -y dirb
+sudo apt install -y wpscan
+sudo apt install -y sqlmap
+sudo apt install -y burpsuite
+sudo apt install -y metasploit
+sudo apt install -y owasp-zap
+sudo apt install -y wpscanteam/tap/wpscan
+sudo apt install -y exploitdb
+sudo apt install -y sambabrew install -y binwalk
+sudo apt install -y exiftool
+sudo apt install -y airckrack-ng
+sudo apt install -y john
 
 
 
