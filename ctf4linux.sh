850032482f7deccd2b682e8a7becf10f68b749f9
montanizstills <montanizstills@gmail.com>
2024-11-06 15:15:36 -0500

    update metasploit install script;

--- ctf4linux.sh
+++ ctf4linux.sh
@@ -1,27 +1,30 @@
 #!/bin/zsh
 
 sudo apt install -y nmap
-snap install -y enum4linux
+snap install enum4linux
 sudo apt install -y smbclient
 sudo apt install -y smbmap
-sudo apt install -y crackmapexec
+#sudo apt install -y crackmapexec
 sudo apt install -y john
 sudo apt install -y hydra
-sudo apt install -y ghidra
+#sudo apt install -y ghidra
 sudo apt install -y hashcat
 sudo apt install -y nikto
 sudo apt install -y gobuster
 sudo apt install -y dirb
-sudo apt install -y wpscan
+#sudo apt install -y wpscan
 sudo apt install -y sqlmap
-sudo apt install -y burpsuite
-sudo apt install -y metasploit
-sudo apt install -y owasp-zap
-sudo apt install -y wpscanteam/tap/wpscan
-sudo apt install -y exploitdb
-sudo apt install -y sambabrew install -y binwalk
+#sudo apt install -y burpsuite
+# sudo apt install -y metasploit
+curl https://raw.githubusercontent.com/rapid7/metasploit-omnibus/master/config/templates/metasploit-framework-wrappers/msfupdate.erb > msfinstall && \
+  chmod 755 msfinstall && \
+  ./msfinstall
+#sudo apt install -y owasp-zap
+#sudo apt install -y wpscanteam/tap/wpscan
+#sudo apt install -y exploitdb
+#sudo apt install -y sambabrew install -y binwalk
 sudo apt install -y exiftool
-sudo apt install -y airckrack-ng
+#sudo apt install -y airckrack-ng
 sudo apt install -y john
 
 
